aa58ed5a520233ddfada0801a7ba7bf6354709b2
Keith Stanley <iamstan13y@gmail.com>
2023-01-25 17:48:57 +0200

    Connection String Config.

--- Todo.API/Program.cs
+++ Todo.API/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 var app = builder.Build();
 
