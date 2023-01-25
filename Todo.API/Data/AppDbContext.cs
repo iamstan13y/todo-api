4b9adb6319b26185e2ff3e2133106454effbfae1
Keith Stanley <iamstan13y@gmail.com>
2023-01-25 18:36:57 +0200

    Todo Model & DB Migrations.

---
+++ Todo.API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.API.Models;
+
+namespace Todo.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ToDo> ToDos => Set<ToDo>();
+    }
+
+}
