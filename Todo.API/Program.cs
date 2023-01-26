ef3933403224bf8b528bacde4a5c4e7185af8fc8
Keith Stanley <iamstan13y@gmail.com>
2023-01-26 09:10:29 +0200

    Post Todo Implementation.

--- Todo.API/Program.cs
+++ Todo.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Todo.API.Data;
+using Todo.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,14 @@ app.MapGet("api/todo", async (AppDbContext context) =>
     var items = await context.ToDos.ToListAsync();
 
     return Results.Ok(items);
+});
+
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
+{
+    await context.ToDos.AddAsync(toDo);
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/todo/{toDo.Id}", toDo);
 });
 
 app.Run();
